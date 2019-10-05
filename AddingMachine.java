e041af98e41bbc36da37c347c7bf3bf4a2b07601
Xinkai Wang <546296781@qq.com>
2019-10-04 19:59:17 -0700

    initial version

---
+++ AddingMachine.java
@@ -0,0 +1,57 @@
+/**
+ * name: Xinkai Wang  Class#70642  assignment number: 2
+ */
+package cse360assign2;
+
+/**
+ * This class is used for adding and subtracting operations from zero 
+ * and for recording and displaying historical operations.
+ * @author Xinkai Wang
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * get the total value
+	 * @return total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add input value to total and record to history.
+	 * @param value the input value need to be add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * minus value from total and record to history.
+	 * @param value the input value need to be minus
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * return the history of opration.
+	 */
+	public String toString () {
+		return "";
+	}
+	
+   /**
+    * clear the total value and history.
+    */
+	public void clear() {
+	
+	}
+}
